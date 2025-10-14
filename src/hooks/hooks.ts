84382da565208e13a0761913d7a030a03a518a96
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-14 20:28:50 +0300

    fix import path for AppDispatch and RootState in hooks

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,6 +1,6 @@
 import type {TypedUseSelectorHook} from 'react-redux';
 import {useDispatch, useSelector} from 'react-redux';
-import type {AppDispatch, RootState} from './todo-store';
+import type {AppDispatch, RootState} from '../app/todo-store.ts';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
