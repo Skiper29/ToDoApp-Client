d04ff1c51e535dac8950f4d201377323c7258f5a
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-14 16:50:00 +0300

    add TodoTask models and update TypeScript configuration

---
+++ src/models/todo.ts
@@ -0,0 +1,29 @@
+export enum TodoTaskStatus {
+    Todo = 0,
+    InProgress = 1,
+    Done = 2
+}
+
+export interface CreateTodoTaskDto {
+    title: string;
+    content?: string;
+    deadline?: Date;
+}
+
+export interface TodoTaskDto {
+    id: number;
+    title: string;
+    content?: string | null;
+    status: TodoTaskStatus;
+    deadline?: Date | null;
+    createdAt: Date;
+    finishedAt: Date | null;
+}
+
+export interface UpdateTodoTaskDto extends CreateTodoTaskDto{
+    id: number;
+}
+
+export interface UpdateTaskStatusDto {
+    status: TodoTaskStatus;
+}
