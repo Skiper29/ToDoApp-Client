eb83e3fbda5c5070fd5efae9b021ec19b6f5a954
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-15 17:21:22 +0300

    add(App): integrate TodoBoard component into the main application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {CssBaseline, ThemeProvider} from '@mui/material';
 import {darkTheme} from './theme/theme';
+import TodoBoard from "./pages/TodoBoard.tsx";
 
 function App() {
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline/>
+            <TodoBoard/>
         </ThemeProvider>
     );
 }
