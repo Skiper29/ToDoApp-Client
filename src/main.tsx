b284d6a6af6088195e7e0a9513227549aa48d574
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-15 15:01:08 +0300

    wrap App component with Redux Provider for state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {Provider} from 'react-redux'
+import {todoStore} from './app/todo-store'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={todoStore}>
+            <App/>
+        </Provider>
+    </StrictMode>,
 )
