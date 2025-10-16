1beaf511519f2e477211c4957c74b785d9ec2db4
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-10-16 15:04:11 +0300

    fix(api-routes): switch to process.env for API_BASE_URL and update Vite config for environment variables

--- src/constants/api-routes.constants.ts
+++ src/constants/api-routes.constants.ts
@@ -1,5 +1,5 @@
 export const API_ROUTES = {
-    BASE: import.meta.env.VITE_API_BASE_URL || '',
+    BASE: process.env.API_BASE_URL || '',
     TODO_TASK: {
         BASE: "TodoTask",
     }
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@models": path.resolve(__dirname, "src/models"),
-            "@components": path.resolve(__dirname, "src/components"),
-            "@api": path.resolve(__dirname, "src/api"),
-            "@utils": path.resolve(__dirname, "src/utils"),
-            "@hooks": path.resolve(__dirname, "src/hooks"),
-            "@constants": path.resolve(__dirname, "src/constants"),
-            "@pages": path.resolve(__dirname, "src/pages"),
-            "@theme": path.resolve(__dirname, "src/theme"),
-            "@store": path.resolve(__dirname, "src/store"),
-            "@assets": path.resolve(__dirname, "src/assets"),
-            "@features": path.resolve(__dirname, "src/features"),
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    const processEnv = Object.keys(env)
+        .filter((key) => key.startsWith("VITE_"))
+        .reduce((acc, key) => {
+            const newKey = key.replace(/^VITE_/, "");
+            acc[`process.env.${newKey}`] = JSON.stringify(env[key]);
+            return acc;
+        }, {} as Record<string, string>);
+
+    return {
+        plugins: [react()],
+        define: processEnv,
+        resolve: {
+            alias: {
+                "@models": path.resolve(__dirname, "src/models"),
+                "@components": path.resolve(__dirname, "src/components"),
+                "@api": path.resolve(__dirname, "src/api"),
+                "@utils": path.resolve(__dirname, "src/utils"),
+                "@hooks": path.resolve(__dirname, "src/hooks"),
+                "@constants": path.resolve(__dirname, "src/constants"),
+                "@pages": path.resolve(__dirname, "src/pages"),
+                "@theme": path.resolve(__dirname, "src/theme"),
+                "@store": path.resolve(__dirname, "src/store"),
+                "@assets": path.resolve(__dirname, "src/assets"),
+                "@features": path.resolve(__dirname, "src/features"),
+            },
         },
-    },
+    };
 });
